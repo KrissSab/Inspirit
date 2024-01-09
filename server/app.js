2c04414f971207934b9167056463c61dfe3ed6e0
Kriss <kris1stsab@gmail.com>
2024-01-09 23:36:32 +0200

    chore | +formated code by prettier

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authentication');
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authentication");
 
 const app = express();
 const PORT = 3001;
 
 app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port 3001`)
-})
-
+  console.log(`Server running on port 3001`);
+});
--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,61 +1,62 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const database = require('../database.js')
+const express = require("express");
+const bcrypt = require("bcrypt");
+const database = require("../database.js");
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    const { username, email, password, birth_date, address, phone_number } = req.body
+router.post("/register", async (req, res) => {
+  const { username, email, password, birth_date, address, phone_number } =
+    req.body;
 
-    const passwordHash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(password, 10);
 
-    const db = database.initDatabase();
-    const insertValues = `
+  const db = database.initDatabase();
+  const insertValues = `
         INSERT INTO users (username, email, password_hash, birth_date, address, phone_number)
         VALUES (?,?,?,?,?,?)
     `;
 
-    db.run(insertValues, [username, email, passwordHash, birth_date, address, phone_number], (err) => {
-        if (err) {
-            console.error(`Error with inserting user: ${err.message}`);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-        else {
-            console.log('User registered successfully');
-            res.status(201).json({ message: 'User registered successfully' });
-        }
-        database.closeDatabase(db);
-    })
-})
-
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    const db = database.initDatabase();
-    const selectQuery = 'SELECT * FROM users WHERE email = ?';
-
-    db.get(selectQuery, [email], async (err, user) => {
-        if (err) {
-            console.error(`Error with retrieving user ${err.message}`);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-        else if (!user) {
-            console.log('User not found');
-            res.status(401).json({ error: 'Invalid email or password' })
-        }
-        else {
-            const passwordMatch = await bcrypt.compare(password, user.password_hash);
-            if (passwordMatch) {
-                console.log('User logged in successfully');
-                res.status(201).json({ message: 'Login successful' });
-            }
-            else {
-                console.log('Invalid password');
-                res.status(401).json({ error: 'Invalid email or password' });
-            }
-        }
-        database.closeDatabase(db);
-    });
+  db.run(
+    insertValues,
+    [username, email, passwordHash, birth_date, address, phone_number],
+    (err) => {
+      if (err) {
+        console.error(`Error with inserting user: ${err.message}`);
+        res.status(500).json({ error: "Internal Server Error" });
+      } else {
+        console.log("User registered successfully");
+        res.status(201).json({ message: "User registered successfully" });
+      }
+      database.closeDatabase(db);
+    },
+  );
 });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const db = database.initDatabase();
+  const selectQuery = "SELECT * FROM users WHERE email = ?";
+
+  db.get(selectQuery, [email], async (err, user) => {
+    if (err) {
+      console.error(`Error with retrieving user ${err.message}`);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else if (!user) {
+      console.log("User not found");
+      res.status(401).json({ error: "Invalid email or password" });
+    } else {
+      const passwordMatch = await bcrypt.compare(password, user.password_hash);
+      if (passwordMatch) {
+        console.log("User logged in successfully");
+        res.status(201).json({ message: "Login successful" });
+      } else {
+        console.log("Invalid password");
+        res.status(401).json({ error: "Invalid email or password" });
+      }
+    }
+    database.closeDatabase(db);
+  });
+});
+
+module.exports = router;
